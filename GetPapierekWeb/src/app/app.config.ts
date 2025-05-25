0315c80fad416a4cbdd46aef4be46e6d06a3543a
wojd0 <wojciechduda03@gmail.com>
2025-05-25 20:30:16 +0200

    code formatting

--- GetPapierekWeb/src/app/app.config.ts
+++ GetPapierekWeb/src/app/app.config.ts
@@ -1,6 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 import { routes } from './app.routes';
 
@@ -8,6 +11,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
   ],
 };
